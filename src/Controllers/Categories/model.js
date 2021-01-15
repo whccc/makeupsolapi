44a10fcabc9327ad3c9444eedf5b0bb81f704964
Wilson Castro <wilsoncastro0710@gmail.com>
2021-01-15 13:38:54 -0500

    SubCategory

--- src/Controllers/Categories/model.js
+++ src/Controllers/Categories/model.js
@@ -1,30 +1,45 @@
-const Categories = require('./modelmongodb')
+const Categories = require("./modelmongodb");
 
 module.exports = {
-  async MDSave ({ strName }) {
+  async MDSave({ strName }) {
     // Create Category
     const Category = new Categories({
-      strName
-    })
-    await Category.save()
+      strName,
+    });
+    await Category.save();
   },
-  async MDFindOne (strData) {
+  async MDFindOne(strData) {
     // Find Category
-    const objResponse = await Categories.findOne(strData)
-    return objResponse
+    const objResponse = await Categories.findOne(strData);
+    return objResponse;
   },
-  async MDFind () {
+  async MDFind() {
     // Get Categories
-    const objRes = await Categories.find({ }, { strName: 1 })
-    return objRes
+    const objRes = await Categories.find({}, { strName: 1 }).sort({
+      $natural: -1,
+    });
+    return objRes;
   },
-  async MDFindPagination ({ Min = 0, Max = 10 }) {
-    const objRes = await Categories.find({}, { strName: 1 }).sort({ $natural: -1 }).skip(Min).limit(Max)
-    return objRes
+  async MDFindPagination({ Min = 0, Max = 10 }) {
+    const objRes = await Categories.find({}, { strName: 1 })
+      .sort({ $natural: -1 })
+      .skip(Min)
+      .limit(Max);
+    return objRes;
   },
   // Update
-  async MDFindIdUpdate ({ _id, strText }) {
-    const objRes = await Categories.findByIdAndUpdate({ _id }, { strName: strText })
-    return objRes
-  }
-}
+  async MDFindIdUpdate({ _id, strText }) {
+    const objRes = await Categories.findByIdAndUpdate(
+      { _id },
+      { strName: strText }
+    );
+    return objRes;
+  },
+  async MDDeleteCategory({ _id }) {
+    try {
+      await Categories.findByIdAndRemove({ _id });
+    } catch (Error) {
+      console.log(Error);
+    }
+  },
+};
--- src/Controllers/SubCategories/routes.js
+++ src/Controllers/SubCategories/routes.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const { CreateSubCategory } = require('./controller')
+const express = require("express");
+const router = express.Router();
+const {
+  CreateSubCategory,
+  GetSubCategoriesByCategories,
+  PutSubCategory,
+} = require("./controller");
 
-router.route('/')
-  .post(CreateSubCategory)
+router.route("/").post(CreateSubCategory).put(PutSubCategory);
 
-module.exports = router
+router.route("/:strIdCategory").get(GetSubCategoriesByCategories);
+
+module.exports = router;
