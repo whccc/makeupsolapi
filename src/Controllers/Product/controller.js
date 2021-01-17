897910496b978c35337f3d5f4d713577a2e6d4ae
Wilson Castro <wilsoncastro0710@gmail.com>
2021-01-17 13:38:47 -0500

    se crea modulo productos

--- src/Controllers/Product/controller.js
+++ src/Controllers/Product/controller.js
@@ -1,49 +1,93 @@
-const MDProduct = require('./model')
-const Helpers = require('../../Helpers/utils')
+const MDProduct = require("./model");
+const Helpers = require("../../Helpers/utils");
 // Validate Data
 const ValidateData = (strData) => {
-  if (Helpers.ValidateData(strData.strCode)) {
-    return Helpers.objResponse(false, 'Empty field strCode is required')
-  }
   if (Helpers.ValidateData(strData.strName)) {
-    return Helpers.objResponse(false, 'Empty field strName is required')
+    return Helpers.objResponse(false, "Empty field strName is required");
   }
   if (Helpers.ValidateData(strData.strDescription)) {
-    return Helpers.objResponse(false, 'Empty field strDescription is required')
+    return Helpers.objResponse(false, "Empty field strDescription is required");
   }
   if (Helpers.ValidateData(strData.strPrice)) {
-    return Helpers.objResponse(false, 'Empty field strPrice is required')
+    return Helpers.objResponse(false, "Empty field strPrice is required");
   }
   if (Helpers.ValidateData(strData.strIdCategory)) {
-    return Helpers.objResponse(false, 'Empty field strIdCategory is required')
+    return Helpers.objResponse(false, "Empty field strIdCategory is required");
   }
   if (Helpers.ValidateData(strData.strIdSubCategory)) {
-    return Helpers.objResponse(false, 'Empty field strIdSubCategory is required')
+    return Helpers.objResponse(
+      false,
+      "Empty field strIdSubCategory is required"
+    );
   }
   if (strData.ArrayImages === undefined) {
-    return Helpers.objResponse(false, 'Field ArrayImages is required')
+    return Helpers.objResponse(false, "Field ArrayImages is required");
   }
   if (strData.ArrayImages.length === 0) {
-    return Helpers.objResponse(false, 'Empty field ArrayImages')
+    return Helpers.objResponse(false, "Empty field ArrayImages");
   }
-  return Helpers.objResponse(true, 'Fields with success')
-}
+  return Helpers.objResponse(true, "Fields with success");
+};
 module.exports = {
-  async CreateProduct (req, res) {
-    let objResponse = {}
-    // Validate Date
-    const objValidate = ValidateData(req.body)
-    if (!objValidate.Success) {
-      res.json(objValidate)
+  async CreateProduct(req, res) {
+    try {
+      let objResponse = {};
+      // Validate Date
+      const objValidate = ValidateData(req.body);
+      console.log(objValidate);
+      if (!objValidate.Success) {
+        res.json(objValidate);
+        return;
+      }
+      // Validate Code
+      await MDProduct.MDSaveProduct(req.body);
+      objResponse = Helpers.objResponse(true, "Product create with success");
+
+      res.json(objResponse);
+    } catch (Error) {
+      console.log(Error);
     }
-    // Validate Code
-    const objDuplicate = await MDProduct.MDFindOne(req.body)
-    if (objDuplicate == null) {
-      await MDProduct.MDSaveProduct(req.body)
-      objResponse = Helpers.objResponse(true, 'Product create with success')
-    } else {
-      objResponse = Helpers.objResponse(false, 'Product strCode duplicate')
+  },
+  async GetProducts(req, res) {
+    try {
+      const Data = await MDProduct.MDGetProducts();
+      res.json(Data);
+    } catch (Error) {
+      console.log(Error);
     }
-    res.json(objResponse)
-  }
-}
+  },
+  async GetProductLimitTen(req, res) {
+    try {
+      const Data = await MDProduct.MDFindPagination(req.body);
+      const Count = await MDProduct.MDCountProducts();
+      res.json({ Products: Data, Count });
+    } catch (Error) {
+      console.log(Error);
+    }
+  },
+  async GetProductsSearch(req, res) {
+    try {
+      const Data = await MDProduct.MdFindProductText(req.body);
+      res.json({ Products: Data });
+    } catch (Error) {
+      console.log(Error);
+    }
+  },
+  async DeleteProduct(req, res) {
+    try {
+      await MDProduct.MdDeleteById(req.body);
+      res.json({ Success: true });
+    } catch (Error) {
+      console.log(Error);
+    }
+  },
+  async EditProduct(req, res) {
+    try {
+      await MDProduct.MDUpdateProduct(req.body);
+      res.json({ Success: true });
+    } catch (Error) {
+      console.log(Error);
+      res.json({ Success: false });
+    }
+  },
+};
--- src/Controllers/Product/routes.js
+++ src/Controllers/Product/routes.js
@@ -1,8 +1,21 @@
-const express = require('express')
-const router = express.Router()
-const { CreateProduct } = require('./controller')
+const express = require("express");
+const router = express.Router();
+const {
+  CreateProduct,
+  GetProducts,
+  GetProductLimitTen,
+  GetProductsSearch,
+  DeleteProduct,
+  EditProduct,
+} = require("./controller");
 
-router.route('/')
+router
+  .route("/")
   .post(CreateProduct)
+  .get(GetProducts)
+  .delete(DeleteProduct)
+  .put(EditProduct);
+router.route("/Limit").post(GetProductLimitTen);
+router.route("/Filter").post(GetProductsSearch);
 
-module.exports = router
+module.exports = router;
