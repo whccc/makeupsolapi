fc9778895ec6c614ebffc8c4de968c2bee300973
Wilson Castro <wilsoncastro0710@gmail.com>
2021-01-19 12:41:22 -0500

    puerto

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const ConnectionDb = require('./ConectionDB/MongoDb')
-const { appConfig, db } = require('./Config/Config')
-const indexRouter = require('./Routes/index')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const ConnectionDb = require("./ConectionDB/MongoDb");
+const { appConfig, db } = require("./Config/Config");
+const indexRouter = require("./Routes/index");
+const app = express();
 
 // Middlewares
-app.use(express.json({ limit: '20mb' }))
-app.use(cors())
+app.use(express.json({ limit: "20mb" }));
+app.use(cors());
 
 // Base de datos
-ConnectionDb(db)
+ConnectionDb(db);
 
 // Rutas
-app.use('/api', indexRouter)
+app.use("/api", indexRouter);
 // Escuchar servidor puerto
-app.listen(appConfig.port, () => {
-  console.log('Server escuchando por ' + appConfig.port)
-})
+app.listen(process.env.PORT | appConfig.port, () => {
+  console.log("Server escuchando por " + appConfig.port);
+});
