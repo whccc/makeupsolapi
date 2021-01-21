44f53bf9c2975bcb41b0a68f396662e76dac8dc8
Wilson Castro <wilsoncastro0710@gmail.com>
2021-01-21 15:01:51 -0500

    Config

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -3,11 +3,11 @@ const Config = {
     port: process.env.APP_PORT,
   },
   db: {
-    host: process.env.DB_HOST,
+    host: "@cluster.hrdqg.mongodb.net",
     port: process.env.DB_PORT,
-    dbName: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    user: process.env.DB_USER,
+    dbName: "makeupsol",
+    password: "makeupsol",
+    user: "admin",
   },
 };
 
